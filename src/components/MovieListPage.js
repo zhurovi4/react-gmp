982aee479d7b85d9603c855701be5661b98b98b4
Valery Zhurovich <valery_zhurovich@epam.com>
2023-05-14 22:19:10 +0200

    Added 'back to search' button

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -53,74 +53,27 @@ export const MovieListPage = () => {
   }, [searchQuery, sortingCriterion, selectedGenre]);
 
   const handleSearch = (searchQuery) => {
-    //console.log("Performing search for query:", searchQuery);
     setSearchQuery(searchQuery);
   };
 
   const handleMovieTileClick = (movie) => {
-    console.log(selectedMovie);
-    console.log(movie);
     setSelectedMovie(movie);
   };
-
-
-  //   const movies2 = [
-  //     {
-  //       id: 1,
-  //       poster_path:
-  //         "https://m.media-amazon.com/images/M/MV5BMTE5ZmIwNjEtZmZjYS00NmM0LTgyNDUtNjFjMzM3MDc1NWM2XkEyXkFqcGdeQXVyNTE1MDE2MzY@._V1_QL75_UX380_CR0,4,380,562_.jpg",
-  //       name: "Pulp Fiction",
-  //       release_date: 1994,
-  //       vote_average: 8.9,
-  //       overview:
-  //         "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-  //       genres: ["Crime", "Drama"],
-  //     },
-  //     {
-  //       id: 2,
-  //       poster_path:
-  //         "https://m.media-amazon.com/images/M/MV5BYTE2NDI0ZjgtZmI2Ni00NDI4LTgwZTctN2I3OWIzNjhhYjY1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg",
-  //       name: "Reservoir Dogs",
-  //       release_date: 1992,
-  //       vote_average: 8.3,
-  //       overview:
-  //         "When a simple jewelry heist goes horribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
-  //       duration: "1h 39 min",
-  //       genres: ["Crime", "Thriller"],
-  //     },
-  //     {
-  //       id: 3,
-  //       poster_path:
-  //         "https://m.media-amazon.com/images/M/MV5BMTkxOTMyMDI0MV5BMl5BanBnXkFtZTcwODU0OTk1Mw@@._V1_.jpg",
-  //       name: "Inception",
-  //       release_date: 2010,
-  //       vote_average: 8.8,
-  //       overview:
-  //         "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
-  //       duration: "2h 28 min",
-  //       genres: ["Action", "Adventure", "Sci-Fi"],
-  //     },
-  //     {
-  //       id: 4,
-  //       poster_path:
-  //         "https://m.media-amazon.com/images/M/MV5BNmFiYmJmN2QtNWQwMi00MzliLThiOWMtZjQxNGRhZTQ1MjgyXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
-  //       name: "Kill Bill: Vol. 2",
-  //       release_date: 2004,
-  //       vote_average: 8.8,
-  //       overview:
-  //         "The Bride continues her quest of vengeance against her former boss and lover Bill, the reclusive bouncer Budd, and the treacherous, one-eyed Elle.",
-  //       duration: "2h 17 min",
-  //       genres: ["Action", "Adventure", "Sci-Fi"],
-  //     },
-  //   ];
 
   const handleGenreSelect = (genre) => {
     setSelectedGenre(genre);
   };
+
+  const closeMovieDetails = () => {
+    setSelectedMovie(null)
+  }
   return (
     <>
       {selectedMovie ? (
+        <>
+        <button className={styles.searchButton} onClick={closeMovieDetails}></button>
         <MovieDetails movie={selectedMovie} />
+        </>
       ) : (
         <Header>
           <SearchForm initialQuery={searchQuery} onSearch={handleSearch} />
